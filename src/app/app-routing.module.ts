e79c2021a78d2c27b929bfceffde7c926cf959e4
Martin Diaz <mfdiaz76@gmail.com>
2020-12-08 14:55:22 -0300

    Se suben los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent} from './pages/main/main.component';
+import { NosotrosComponent} from './pages/nosotros/nosotros.component';
+import { ServiciosComponent} from './pages/servicios/servicios.component';
+import { PortfolioComponent} from './pages/portfolio/portfolio.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  /*Äca van todas las rutas que la aplicacion va a tener*/
+  {path: 'inicio', component: MainComponent},
+  {path: 'servicios', component: ServiciosComponent},
+  {path: 'nosotros', component: NosotrosComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'inicio'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MainComponent } from './pages/main/main.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    MainComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
